ec2a65c5377ccb1adb0a61a0bce6788dba7fd304
Brandon Wiley <brandon@blanu.net>
2015-01-27 17:54:46 -0600

    Generate images instead of graphs

--- graph-seqs.py
+++ graph-seqs.py
@@ -1,7 +1,8 @@
+import os
 import json
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+import Image
+import colorsys
 
 def splitData(data):
   xs=[]
@@ -16,14 +17,37 @@ def splitData(data):
       zs.append(z)
   return (xs, ys, zs)
 
-f=open('seqs/protocols/HTTPRSS')
-data=f.read()
-f.close()
+def graph(data, direction, protocol):
+  ymax=len(data)
+  xmax=len(data[0])
 
-data=json.loads(data)
-xs, ys, zs=splitData(data['incoming'])
+  img = Image.new( 'RGB', (ymax,xmax), "black") # create a new black image
+  pixels = img.load() # create the pixel map
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.bar3d(xs, ys, zs)
-plt.savefig('HTTPRSS-incoming.pdf')
+  for y in range(ymax):    # for every pixel:
+    for x in range(xmax):
+      z=data[y][x]
+      if z==1:
+        pixels[y,x] = (255, 255, 255)
+      elif z==0:
+        pixels[y,x] = 0
+      else:
+        r, g, b = colorsys.hsv_to_rgb(z*(2.0/3.0), 1.0, 1.0)
+        pixels[y,x] = (int(r*255), int(g*255), int(b*255))
+
+#  img.show()
+  img.save('seq-graphs/'+protocol+'-'+direction+'.png')
+
+if not os.path.exists('seq-graphs'):
+  os.mkdir('seq-graphs')
+for protocol in os.listdir('seqs/protocols'):
+  print('Loading...')
+  f=open('seqs/protocols/'+protocol)
+  data=f.read()
+  f.close()
+
+  print('Parsing...')
+  data=json.loads(data)
+  print('Splitting...')
+  graph(data['incoming'], 'incoming', protocol)
+  graph(data['outgoing'], 'outgoing', protocol)
